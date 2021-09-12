422c86c6835bf98811cbd816b0f35db354e36bf6
lesid10 <koneaziz587@gmail.com>
2021-09-12 20:29:46 +0200

    creation d activte, ajout de resource, et creation de fragment

---
+++ app/src/main/java/fr/sid/naturecollection/MainActivity.kt
@@ -0,0 +1,19 @@
+package fr.sid.naturecollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.sid.naturecollection.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boite (fragment_container)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
